96fd8a483f172b7f3ac84b2fa131f82060398cc8
Amine Neggazi <neggazimedlamine@gmail.com>
2022-06-19 01:11:17 +0100

    added table7 generator script!

---
+++ PFE/latex/tables/generateTable7.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+
+# # read from data file and get length of words
+# while read -r line; do
+#   words=($line)
+#   for word in "${words[@]}"; do
+#     echo -n '\textbf{'$word'} & '
+#   done
+#   echo '\\ \hline'
+#   len=$(echo $line | wc -w)
+# done < data.txt
+
+# read from data file and get length of words
+while read -r line; do
+  words=($line)
+  for word in "${words[@]}"; do
+    data="${data} \\\textbf{$word} &"
+  done
+  data=$(echo $data | sed 's|.$||g')
+  data+="\\\\\ \\hline\n"
+  len=$(echo $line | wc -w)
+done < data.txt
+data=$(echo $data | sed 's|_|\\\_|g')
+
+columns=''
+# generate the number of columns
+for i in $(seq 1 $len); do
+  columns="${columns}|c"
+done
+columns="${columns}|"
+
+
+# now generate the table
+echo -ne '\\begin{table}[H]
+\\centering
+\\begin{tabular}{'$columns'}
+\\hline\n '$(echo $data)'
+\\end{tabular}
+\\caption{Results of each algorithm}
+\\label{Results of each algorithm}
+\\end{table}
+' > table7.tex
